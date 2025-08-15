d36579ad8a0f4baa74589dd9ef6b1be4e3d139f5
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-15 20:18:08 +0530

    Base setup complete

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+
+
+// Functions
+const connectDB = require('./config/DB');
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+});
